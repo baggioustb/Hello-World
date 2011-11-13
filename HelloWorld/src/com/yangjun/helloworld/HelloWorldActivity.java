2d153f4da1eacfd3c6bed74656247f14b3a8b649
Robert Yang <junyang.ustb@gmail.com>
2011-11-13 14:52:58 +0800

    change: Add the helloworld project

--- HelloWorld/src/com/yangjun/helloworld/HelloWorldActivity.java
+++ HelloWorld/src/com/yangjun/helloworld/HelloWorldActivity.java
@@ -2,12 +2,22 @@ package com.yangjun.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HelloWorldActivity extends Activity {
     /** Called when the activity is first created. */
+	Button btn;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				
+			}
+		});
     }
 }
